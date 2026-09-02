cffa64dfb0b4542404311b490cfc0f5fa0aff47a
Abhinav-grx <abhinavgr81542@gmail.com>
2026-09-02 09:43:38 +0530

    Add program to display even numbers and their sum
    Prints even numbers from 1 to 50 and their sum.

--- Practical_2/practical2.cpp
+++ Practical_2/practical2.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i,sum = 0;
+    cout << "Even numbers between 1 and 50 are:" << endl;
+    
+    for(int i = 2; i <= 50; i += 2) {
+        sum = sum + i;
+         cout <<i<<endl; 
+    }
+    cout << "\n\nThe sum of even numbers between 1 and 50 is : "<<sum<<endl;
+    return 0;
+}
 
