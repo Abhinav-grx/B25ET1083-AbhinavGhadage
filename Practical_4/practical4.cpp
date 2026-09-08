fd20b104f27032271c7e1cb2d5534b0d008e73f4
Abhinav-grx <abhinavgr81542@gmail.com>
2026-09-08 21:36:36 +0530

    Update practical4.cpp

--- Practical_4/practical4.cpp
+++ Practical_4/practical4.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+class Complex
+{
+    float real, imag;
+public:
+    void getdata()
+    {
+        cout << "Enter real part: ";
+        cin >> real;
 
+        cout << "Enter imaginary part: ";
+        cin >> imag;
+    }
+
+    void divide(float scalar)
+    {
+        real = real / scalar;
+        imag = imag / scalar;
+    }
+    void display()
+    {
+        cout << "Result = " << real << " + " << imag << "i";
+    }
+};
+int main()
+{
+    Complex c;
+    float scalar;
+    c.getdata();
+    cout << "Enter scalar value: ";
+    cin >> scalar;
+    c.divide(scalar);
+    c.display()
+    return 0;
+}
